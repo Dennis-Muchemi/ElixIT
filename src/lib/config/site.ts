d2818b9f5b60d822338a00374d960a15096ae918
denno_muchemi <dennismuchemi101@gmail.com>
2025-01-02 12:31:25 +0300

    Initial commit: Project setup with Next.js and base configuration

---
+++ src/lib/config/site.ts
@@ -0,0 +1,10 @@
+export const siteConfig = {
+    name: "ElixIT",
+    description: "Transforming businesses through innovative digital experiences",
+    url: "https://elixit.com",
+    ogImage: "https://elixit.com/og.jpg",
+    links: {
+      twitter: "https://twitter.com/elixit",
+      github: "https://github.com/elixit"
+    }
+  }
