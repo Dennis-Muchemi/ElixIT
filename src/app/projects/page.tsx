6c85af8bdfcff6bea38ce48ac45cc2ef96675eb6
denno_muchemi <dennismuchemi101@gmail.com>
2025-01-10 14:52:43 +0300

    files update

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,9 +1,16 @@
-// src/app/projects/page.tsx
 'use client';
 
 import { ProjectsPage } from '@/components/projects/ProjectsPage';
 import { projects } from '@/data/projectsData';
 
+// Add this type definition right here in the same file
+type Props = {
+  initialProjects: typeof projects;
+};
+
+// Add the Props type to ProjectsPage
+const TypedProjectsPage = ProjectsPage as React.ComponentType<Props>;
+
 export default function Page() {
-  return <ProjectsPage initialProjects={projects} />;
+  return <TypedProjectsPage initialProjects={projects} />;
 }
